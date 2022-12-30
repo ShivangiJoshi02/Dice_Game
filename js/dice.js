cd1fe70e17cd9be34dde11feec111caacfa5ddf5
ShivangiJoshi02 <106532382+ShivangiJoshi02@users.noreply.github.com>
2022-12-31 01:07:07 +0530

    Update dice.js

--- js/dice.js
+++ js/dice.js
@@ -11,12 +11,12 @@ var randomimg2 = "images/dice" + randomNum2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomimg2);
 
 if (randomNum1 > randomNum2) {
-    document.querySelector("h2").innerHTML = "🚩 Play 1 Wins!";
+    document.querySelector("h2").innerHTML = "🚩 Play 1 Wins!<br> Refresh to throw the dice.";
   }
   else if (randomNum2 > randomNum1) {
-    document.querySelector("h2").innerHTML = "Player 2 Wins! 🚩";
+    document.querySelector("h2").innerHTML = "Player 2 Wins! 🚩<br> Refresh to throw the dice.";
   }
   else {
-    document.querySelector("h2").innerHTML = "Draw!...play again";
+    document.querySelector("h2").innerHTML = "Draw!...play again <br> Refresh to throw the dice.";
   }
   
